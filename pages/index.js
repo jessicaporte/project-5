ca65cfa0222e56e442c63c4c1d724d7c84265291
jessica <jessy_marian91@hotmail.com>
2025-04-15 09:42:56 +0200

    1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import useSWR from "swr";
+
+const URL = `https://example-apis.vercel.app/api/art`;
+
+const fetcher = (URL) =>
+  fetch(URL).then((response) => {
+    //cargando el api
+    if (!response.ok) {
+      throw new Error("error!");
+    }
+    return response.json();
+  });
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading) return <p> Loading... </p>;
+  if (error) return <p>error</p>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>ART GALLERY</h1>
+      <ul>
+        {" "}
+        {data.map((artPiece) => (
+          <li key={artPiece.slug}>
+            <img src={artPiece.imageSource} alt={artPiece.name} width={200} />
+            <h2>{artPiece.name}</h2>
+            <p>{artPiece.artist}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
